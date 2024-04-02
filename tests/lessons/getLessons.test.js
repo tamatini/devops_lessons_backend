9db3b9c8ed6e0b5896b2866c96a0c9e297907a1f
Jeffrey Teahui <jeffrey.teahui@afpa-montpellier.fr>
2024-04-02 07:29:15 +0200

    add lessions tests

--- tests/lessons/getLessons.test.js
+++ tests/lessons/getLessons.test.js
@@ -24,14 +24,28 @@ describe('GET /lessons', () => {
     });
 
     it ('should return a list of lessons', async() => {
-        postLesson('Lesson 1');
+        await postLesson('Lesson 1');
         const res = await request(app).get(url); 
         expect(res.body).to.be.an('array');
     });
 
+    it ('should return a emty array if no lessons', async() => {
+        const res = await request(app).get(url);
+        expect(res.body).to.be.an('array').that.is.empty;
+    });
+
     it ('array should contain a lesson object', async() => {
-        postLesson('Lesson 1');
+        await postLesson('Lesson 1');
         const res = await request(app).get(url);
         expect(res.body[0]).to.be.an('object');
+    });
+
+    it ('lesson should have good format', async() => {
+        await postLesson('Lesson 1');
+        const res = await request(app).get(url);
+        expect(res.body[0]).to.have.property('_id');
+        expect(res.body[0]).to.have.property('title', 'Lesson 1');
+        expect(res.body[0]).to.have.property('content', 'This is a test lesson');
+        expect(res.body[0]).to.have.property('isPublished', false);
     });
 });
