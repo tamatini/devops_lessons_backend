89c452a5c8d09030b48b4a8801f5d5dac84da1e4
Jeffrey Teahui <jeffrey.teahui@afpa-montpellier.fr>
2024-04-04 06:38:24 +0200

    padded ost lesson function

--- src/routes/lesson.routes.js
+++ src/routes/lesson.routes.js
@@ -1,8 +1,9 @@
-const Router = require('express').Router();
-const lessonController = require('../controllers/lesson.controller');
+const Router = require("express").Router();
+const lessonController = require("../controllers/lesson.controller");
 
-Router.get('/', lessonController.getLessons);
-Router.get('/hello', lessonController.helloLesson);
-Router.get('/:id', lessonController.getSingleLesson);
+Router.get("/", lessonController.getLessons);
+Router.post("/", lessonController.postLesson);
+Router.get("/hello", lessonController.helloLesson);
+Router.get("/:id", lessonController.getSingleLesson);
 
 module.exports = Router;
