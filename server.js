2f825d8ec12b0c0ed356487880bf71c1cf0ce495
Jeffrey Teahui <jeffrey.teahui@afpa-montpellier.fr>
2024-04-04 22:47:58 +0200

    updating database connection

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const PORT = 3000;
+const database = require("./src/database/connect.database");
 
 const start = () => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    }
-)}
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  database.connect("lesson", "localhost", "admin", "password", "8081");
+};
 
 if (require.main === module) {
-    start();
+  start();
 }
