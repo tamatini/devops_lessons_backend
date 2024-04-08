2aad929bbc9cd1ee816023d3e89c84694fd14bed
Jeffrey Teahui <jeffrey.teahui@afpa-montpellier.fr>
2024-04-08 11:11:24 +0200

    fixed logging error

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "logs/access.log"), { flags: "a" });
 const stdoutLogStream = fs.createWriteStream(path.join(__dirname, "logs/stdout.log"), { flags: "a" });
 const errorLogStream = fs.createWriteStream(path.join(__dirname, "logs/error.log"), { flags: "a" });
-app.use(morgan("common", { stream: accessLogStream }));
 
 if (env.NODE_ENV === "production") {
+    app.use(morgan("common", { stream: accessLogStream }));
     process.stdout.write = stdoutLogStream.write.bind(stdoutLogStream);
     process.stderr.write = errorLogStream.write.bind(errorLogStream);
 }
