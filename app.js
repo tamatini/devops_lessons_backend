90aa0609eb96fc93430614649f4195759cfda26a
Jeffrey Teahui <jeffrey.teahui@afpa-montpellier.fr>
2024-04-08 10:38:06 +0200

    added logging

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./src/routes/routes");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
+// Logging configuration
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "logs/access.log"), { flags: "a" });
+const stdoutLogStream = fs.createWriteStream(path.join(__dirname, "logs/stdout.log"), { flags: "a" });
+const errorLogStream = fs.createWriteStream(path.join(__dirname, "logs/error.log"), { flags: "a" });
+app.use(morgan("common", { stream: accessLogStream }));
+process.stdout.write = stdoutLogStream.write.bind(stdoutLogStream);
+process.stderr.write = errorLogStream.write.bind(errorLogStream);
+
+
+// Origin configuration
 app.use(cors());
+
+// Application configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes configuration
 app.use(routes);
 
 module.exports = app;
